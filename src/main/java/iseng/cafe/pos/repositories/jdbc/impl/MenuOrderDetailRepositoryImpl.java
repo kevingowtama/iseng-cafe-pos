7a16c06f6c787bdc48c2837574eef5c91bf768c9
Kevin Gowtama <gowtamakevin@gmail.com>
2021-02-15 14:40:55 +0700

    Fixed menu order detail

--- src/main/java/iseng/cafe/pos/repositories/jdbc/impl/MenuOrderDetailRepositoryImpl.java
+++ src/main/java/iseng/cafe/pos/repositories/jdbc/impl/MenuOrderDetailRepositoryImpl.java
@@ -19,16 +19,16 @@ public class MenuOrderDetailRepositoryImpl implements MenuOrderDetailRepository 
     @Override
     public MenuOrderDetail save(MenuOrderDetail entity) throws DataAccessException {
         if(entity.getId() == null){
-            String query = "INSERT INTO menu_order_detail(menu_order_id, snack_id, quantity, sub_total, created_at, updated_at) " +
-                    "values (?, ?, ?, ?, ?, ?,)";
+            String query = "INSERT INTO menu_order_detail(menu_order_id, snack_id, quantity, sub_total, notes, created_at, updated_at) " +
+                    "values (?, ?, ?, ?, ?, ?, ?)";
             jdbcTemplate.update(query, entity.getMenuOrder().getId(), entity.getSnack().getId(), entity.getQuantity(),
-                     entity.getSubTotal(), LocalDateTime.now(), LocalDateTime.now());
+                     entity.getSubTotal(), entity.getNotes(), LocalDateTime.now(), LocalDateTime.now());
         }
         else{
-            String query = "UPDATE menu_order_detail SET name = ?, email = ?, username = ?, password = ?, menu_order_detail_type_id = ?, updated_at = ?" +
+            String query = "UPDATE menu_order_detail SET snack_id = ?, menu_order_id = ?, quantity = ?, sub_total = ?, notes = ?, updated_at = ? " +
                     "WHERE id = ?";
-            jdbcTemplate.update(query, entity.getMenuOrder().getId(), entity.getSnack().getId(), entity.getQuantity(),
-                    entity.getSubTotal(), LocalDateTime.now(), entity.getId());
+            jdbcTemplate.update(query, entity.getSnack().getId(), entity.getMenuOrder().getId(), entity.getQuantity(),
+                    entity.getSubTotal(), entity.getNotes(), LocalDateTime.now(), entity.getId());
         }
         return entity;
     }
@@ -44,9 +44,9 @@ public class MenuOrderDetailRepositoryImpl implements MenuOrderDetailRepository 
     public MenuOrderDetail findById(Integer id) throws DataAccessException {
         List<MenuOrderDetail> list = jdbcTemplate.query(
             "SELECT menu_order_detail.id, menu_order_detail.quantity, menu_order_detail.sub_total, menu_order_detail.created_at, " +
-                    "menu_order_detail.updated_at, menu_order_detail.notes snack.name, snack.price, menu_order.id FROM menu_order_detail " +
+                    "menu_order_detail.updated_at, menu_order_detail.notes, snack.name, snack.price, menu_order.id FROM menu_order_detail " +
                     "JOIN menu_order ON menu_order_detail.menu_order_id = menu_order.id " +
-                    "Join snack ON menu_order_detail.snack_id = snack.id" +
+                    "Join snack ON menu_order_detail.snack_id = snack.id " +
                     "WHERE menu_order_detail.id=?", (rs, i) -> {
                 MenuOrderDetail menuOrderDetail = new MenuOrderDetail();
                 MenuOrder menuOrder = new MenuOrder();
@@ -77,7 +77,7 @@ public class MenuOrderDetailRepositoryImpl implements MenuOrderDetailRepository 
     public List<MenuOrderDetail> findAll() throws DataAccessException {
         List<MenuOrderDetail> list = jdbcTemplate.query(
             "SELECT menu_order_detail.id, menu_order_detail.quantity, menu_order_detail.sub_total, menu_order_detail.created_at, " +
-                    "menu_order_detail.updated_at, menu_order_detail.notes snack.name, snack.price, menu_order.id FROM menu_order_detail " +
+                    "menu_order_detail.updated_at, menu_order_detail.notes, snack.name, snack.price, menu_order.id FROM menu_order_detail " +
                     "JOIN menu_order ON menu_order_detail.menu_order_id = menu_order.id " +
                     "Join snack ON menu_order_detail.snack_id = snack.id", (rs, i) -> {
                 MenuOrderDetail menuOrderDetail = new MenuOrderDetail();
